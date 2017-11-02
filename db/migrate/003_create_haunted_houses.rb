b7fa252c39ff8d7936843cecd203537505f3a374
Hannah Danziger <hdanziger8@gmail.com>
2017-11-02 19:15:06 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,6 +10,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.datetime :opening_date
       t.datetime :closing_date
       t.text :long_description
+      t.timestamps
     end
   end
 end
