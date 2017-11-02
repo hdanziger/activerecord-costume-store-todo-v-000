f3191d34229fdb303303e9b0f96deaf708c03e6a
Hannah Danziger <hdanziger8@gmail.com>
2017-11-02 18:51:40 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,8 +9,6 @@ class CostumeStore < ActiveRecord::Migration
       t.boolean :in_business?
       t.datetime :opening_time
       t.datetime :closing_time
-
-      t.timestamps null: false
     end
   end
 end
